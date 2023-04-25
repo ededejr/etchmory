f0fe34dd953cabbbd006443d5625ff11dc9e6482
Edede Oiwoh <eoiwoh@gmail.com>
2023-04-25 17:10:47 -0400

    chore: update library exports

---
+++ src/structures/linked-lists/index.ts
@@ -0,0 +1,1 @@
+export { LinearMemory } from "./linear-memory";
--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,27 @@
 import { ERROR_PREFIX } from "./constants";
+import { SourceValue } from "./types";
 
 export function throwError(err: Error) {
   const formattedError = new Error(`${ERROR_PREFIX} ${err.message}`);
   formattedError.stack = err.stack;
   throw formattedError;
+}
+
+/**
+ * Coerces a string value into a SourceValue.
+ */
+export function parseSourceValue(value: string): SourceValue {
+  const isBoolean = ["true", "false"].includes(value);
+
+  if (isBoolean) {
+    return value === "true";
+  }
+
+  const isNumber = !isNaN(Number(value));
+
+  if (isNumber) {
+    return Number(value);
+  }
+
+  return value;
 }
