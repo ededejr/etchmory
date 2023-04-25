2f50e4aea50109e1c375bc8a48865a3c7f9f9b3e
Edede Oiwoh <eoiwoh@gmail.com>
2023-04-25 17:10:47 -0400

    chore: update library exports

--- src/tests/etchmory.test.ts
+++ src/tests/etchmory.test.ts
@@ -1,73 +1,19 @@
-import { Etchmory } from "..";
+import * as Etchmory from "..";
 
 describe("Etchmory", () => {
-  describe("basic functionality", () => {
-    test("can be constructed", () => {
-      const memory = new Etchmory();
-      expect(memory).toBeDefined();
-    });
-
-    test("can mark a decision", () => {
-      const memory = new Etchmory();
-      memory.mark("decision_1", "value_1");
-      memory.complete();
-      expect(memory.recall("decision_1")).toBe("value_1");
-    });
-
-    test("can mark multiple decisions", () => {
-      const memory = new Etchmory();
-      memory.mark("decision_1", "value_1");
-      memory.mark("decision_2", "value_2");
-      memory.complete();
-      expect(memory.recall("decision_1")).toBe("value_1");
-      expect(memory.recall("decision_2")).toBe("value_2");
-    });
-
-    test("can replay decisions in order", () => {
-      const memory = new Etchmory();
-      memory.mark("decision_1", "value_1");
-      memory.mark("decision_2", "value_2");
-      memory.complete();
-      const decisions = memory.replay();
-      expect(decisions.next().value).toBe("value_1");
-      expect(decisions.next().value).toBe("value_2");
-    });
-
-    test("can complete", () => {
-      const memory = new Etchmory();
-      memory.mark("decision_1", "value_1");
-      memory.mark("decision_2", "value_2");
-      memory.complete();
-    });
+  test("exports LinearMemory", () => {
+    expect(Etchmory.LinearMemory).toBeDefined();
   });
 
-  describe("throws relevant errors", () => {
-    test("throws error if replayed before complete", () => {
-      const memory = new Etchmory();
-      memory.mark("decision_1", "value_1");
-      memory.mark("decision_2", "value_2");
-      expect(() => memory.recall("decision_1")).toThrowError(
-        "[Etchmory] Instance must be completed before a repeatable value can be guaranteed"
-      );
-    });
+  test("exports GraphMemory", () => {
+    expect(Etchmory.GraphMemory).toBeDefined();
+  });
 
-    test("throws error if mark is called after complete", () => {
-      const memory = new Etchmory();
-      memory.mark("decision_1", "value_1");
-      memory.mark("decision_2", "value_2");
-      memory.complete();
-      expect(() => memory.mark("decision_3", "value_3")).toThrowError(
-        "[Etchmory] Cannot mark due to instance not being in the active state"
-      );
-    });
+  test("exports UnifiedGraphMemory", () => {
+    expect(Etchmory.UnifiedGraphMemory).toBeDefined();
+  });
 
-    test("throws error if replay is called before complete", () => {
-      const memory = new Etchmory();
-      memory.mark("decision_1", "value_1");
-      memory.mark("decision_2", "value_2");
-      expect(() => memory.recall("decision_1")).toThrowError(
-        "[Etchmory] Instance must be completed before a repeatable value can be guaranteed."
-      );
-    });
+  test("contains no other exports", () => {
+    expect(Object.keys(Etchmory).length).toBe(3);
   });
 });
